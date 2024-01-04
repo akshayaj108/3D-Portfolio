cb82db80ea88ccc0f120fd30979c5203b4a421ce
akshayaj108 <akshayjadhao806@gmail.com>
2024-01-04 18:02:36 +0530

    minor changes in navbar for mobile size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Akshay &nbsp; <span className="sm:block hidden">Jadhao</span>
+            Akshay &nbsp; <span>Jadhao</span>
           </p>
         </Link>
 
