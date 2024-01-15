fc94a4dcc04a31e570e135252c0f604dce8b896e
akshayaj108 <akshayjadhao806@gmail.com>
2024-01-15 08:48:08 +0530

    mnor changes in form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
 
     emailjs
       .send(
-        emailJsTemplateId,
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
@@ -51,7 +51,9 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert(
+            `Thank you${form.name}. I will get back to you as soon as possible.`
+          );
 
           setForm({
             name: "",
